9b1d87d281fa54cbffb64ff7a7ee205af48d024f
not-kaz <kmicagrizli@protonmail.com>
2021-10-05 20:00:00 +0200

    Setup camera in main loop for test purposes.

--- src/application.cpp
+++ src/application.cpp
@@ -2,8 +2,9 @@
 #include <shader.hpp>
 #include <mesh.hpp>
 #include <texture.hpp>
-#include <vector>
+#include <camera.hpp>
 
+#include <vector>
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -16,7 +17,6 @@ application::~application() {}
 void application::main_loop()
 {
     shader s("../shaders/vertex.glsl", "../shaders/frag.glsl");
-    float current_frame = 0, delta_time = 0.0f, last_frame = 0.0f;
 
     std::vector<float> vertices = 
     {
@@ -80,23 +80,24 @@ void application::main_loop()
     mesh m(vertices);
     texture t("../assets/default.jpg");
 
+    s.use();
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)d.get_width() / (float)d.get_height(), 0.1f, 100.0f);
+    s.set_mat4("projection", projection); 
+    
+    camera cam;
+    float current_frame, delta_time, last_frame;
+
     while (this->is_running) 
-    {
-        current_frame = SDL_GetTicks();
+    {            
+        current_frame = SDL_GetTicks() / 1000.0f;
         delta_time = current_frame - last_frame;
         last_frame = current_frame;
 
-        dsp.clear();
+        cam.handle_input(delta_time);
+        d.clear();
         s.use();
 
-        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), 
-            glm::vec3(0.0f, 0.0f, 0.0f), 
-            glm::vec3(0.0f, 1.0f, 0.0f));
-
-        glm::mat4 projection = glm::mat4(1.0f);
-        projection = glm::perspective(glm::radians(45.0f), (float)dsp.get_width() / (float)dsp.get_height(), 0.1f, 100.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -2.0f));
-        s.set_mat4("projection", projection);
+        glm::mat4 view = glm::lookAt(cam.get_pos(), cam.get_pos() + cam.get_front(), cam.get_up());
         s.set_mat4("view", view);
 
         for (unsigned int i = 0; i < 10; i++)
@@ -106,8 +107,8 @@ void application::main_loop()
             float angle = 20.0f * i;
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             s.set_mat4("model", model);
-            m.draw();
+            glDrawArrays(GL_TRIANGLES, 0, 36);
         }
-        dsp.present();
+        d.present();
     }
 }
