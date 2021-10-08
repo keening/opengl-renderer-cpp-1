726161371d66141ef54d5e166d49ef0adcda1613
not-kaz <kmicagrizli@protonmail.com>
2021-10-08 16:10:07 +0200

    Mouse and keyboard input added.

--- include/camera.hpp
+++ include/camera.hpp
@@ -1,16 +1,20 @@
 #include <glm/glm.hpp>
+#include <display.hpp>
 
 class camera
 {
     public:
         camera();
+        camera(int sw, int sh);
         ~camera();
 
         glm::vec3 get_pos() { return position; }
         glm::vec3 get_front() { return front; }
         glm::vec3 get_up() { return up; }
         void handle_input(float delta);
+        void handle_mouse(SDL_Event e);
     private:
-        glm::vec3 position, front, up;
-        float speed;
+        glm::vec3 position, front, up, right;
+        float yaw, pitch, speed, fov, last_x, last_y, sens, velocity;
+        bool is_initial_move;
 };
--- src/camera.cpp
+++ src/camera.cpp
@@ -1,11 +1,14 @@
 #include <camera.hpp>
 #include <SDL.h>
 
-camera::camera() 
+camera::camera(int sw, int sh) : yaw(-90.0f), fov(45.0f), 
+    is_initial_move(true), sens(0.6f), speed(2.5f)
 {
     this->position = glm::vec3(0.0f, 0.0f, 3.0f);
     this->front = glm::vec3(0.0f, 0.0f, -1.0f);
     this->up = glm::vec3(0.0f, 1.0f, 0.0f);
+    this->last_x = sw / 2.0f;
+    this->last_y = sh / 2.0f;
 }
 
 camera::~camera() {}
@@ -13,7 +16,7 @@ camera::~camera() {}
 void camera::handle_input(float delta) 
 {
     SDL_Event event;
-    this->speed = 10.0f * delta;
+    this->velocity = this->speed * delta;
 
     while (SDL_PollEvent(&event))
     {
@@ -23,22 +26,57 @@ void camera::handle_input(float delta)
                 switch (event.key.keysym.sym)
                 {
                     case SDLK_UP:
-                        this->position += this->speed * this->front;
+                        this->position += this->front * this->velocity;
                         break;
                     case SDLK_DOWN:
-                        this->position -= this->speed * this->front;
+                        this->position -= this->front * this->velocity;
                         break;
                     case SDLK_LEFT:
-                        this->position -= this->speed * glm::normalize(
-                            glm::cross(this->front, this->up));
+                        this->position -= this->right * this->velocity;
                         break;
                     case SDLK_RIGHT:
-                        this->position += this->speed * glm::normalize(
-                            glm::cross(this->front, this->up));
+                        this->position += this->right * this->velocity;
                         break;
                     default:
                         break;
                 }
+                break;
+            case SDL_MOUSEMOTION:
+                handle_mouse(event);
+                break;
         }
     }
+}
+
+void camera::handle_mouse(SDL_Event e)
+{
+    if (this->is_initial_move)
+    {
+        last_x = e.motion.x;
+        last_y = e.motion.y;
+        this->is_initial_move = false;
+    }
+
+    int x_offset = e.motion.x - last_x;
+    int y_offset = last_y - e.motion.y;
+    last_x = e.motion.x;
+    last_y = e.motion.y;
+    x_offset *= sens;
+    y_offset *= sens;
+    this->yaw += x_offset;
+    this->pitch += y_offset;
+
+    if (this->pitch > 89.0f)
+        this->pitch = 89.0f;
+    if (this->pitch < -89.0f)
+        this->pitch = -89.0f;
+
+    glm::vec3 tmp;
+    tmp.x = cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+    tmp.y = sin(glm::radians(this->pitch));
+    tmp.z = sin(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+    this->front = glm::normalize(tmp);
+    this->right = glm::normalize(glm::cross(this->front, glm::vec3(0.0f, 1.0f, 0.0f)));
+    this->up = glm::normalize(glm::cross(this->right, this->front));
+
 }
